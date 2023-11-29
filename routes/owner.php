7cc828ff23ee0f862191a7917c28a595c132720a
NeAr0812 <moca0812@icloud.com>
2023-11-29 17:20:56 +0900

    管理側更新

--- routes/owner.php
+++ routes/owner.php
@@ -21,9 +21,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('owner.welcome');
-// });
+Route::get('/', function () {
+    return view('owner.welcome');
+});
 
 
 
